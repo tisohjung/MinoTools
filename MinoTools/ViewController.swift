1f7819a0f5af6eb7a163636b0f7dd7fd8bdf2e87
Minho Yi <minho.yi@riiid.co>
2021-10-16 19:22:13 +0900

    ✨ init project

---
+++ MinoTools/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MinoTools
+//
+//  Created by minho on 2021/10/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
